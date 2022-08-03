7026c5c5aa977752ee79b0392ed9e1ffb3239cad
lAmeR1 <42315864+lAmeR1@users.noreply.github.com>
2022-08-04 00:10:03 +0200

    test box

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
-import { Navbar, Nav, Form, Button } from 'react-bootstrap';
+import { Navbar, Nav, Form, Button, Container, Col, Row } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import BalanceModal from './components/BalanceModal';
 import { useState } from 'react';
@@ -22,33 +22,39 @@ function App() {
     console.log()
     console.log("test", e)
     fetch(`https://kaspa.herokuapp.com/addresses/${e.target.form.searchBox.value}/balance`)
-    .then(response => response.json())
-    .then(data => {
-      setAddress(data["address"]);
-      setBalance(data["balance"]/100000000);
-      setShow(true);
-    })
-    .catch(r => console.log(r))
-    
+      .then(response => response.json())
+      .then(data => {
+        setAddress(data["address"]);
+        setBalance(data["balance"] / 100000000);
+        setShow(true);
+      })
+      .catch(r => console.log(r))
+
   }
-//<Button variant="primary">Go!</Button>
+  //<Button variant="primary">Go!</Button>
   return (
     <div className="App">
-      <Navbar bg="light" expand="lg">
-        <Navbar.Brand>Kaspa EXPLORER</Navbar.Brand>
-        <Nav>
-          <Nav.Link href="#home">Dashboard</Nav.Link>
-          <Nav.Link href="#blocks">Blocks</Nav.Link>
-          <Nav.Link href="#transactions">Transactions</Nav.Link>
-        </Nav>
-      </Navbar>
-      <Form>
-        <Form.Control name="searchBox" type="text" placeholder="Enter kaspa:address / block hash / transaction hash" />
-        <Button variant="primary" onClick={getBalance}>
-          Get $KAS balance DEMO
-        </Button>
-      </Form>
-
+      
+        <Navbar bg="light" expand="lg">
+          <Navbar.Brand>Kaspa EXPLORER</Navbar.Brand>
+          <Nav>
+            <Nav.Link href="#home">Dashboard</Nav.Link>
+            <Nav.Link href="#blocks">Blocks</Nav.Link>
+            <Nav.Link href="#transactions">Transactions</Nav.Link>
+          </Nav>
+        </Navbar><Container fluid>
+        
+            <Form>
+            <Row className="justify-content-md-center">
+          <Col xs={8}>
+            <div>
+              <Form.Control name="searchBox" type="text" placeholder="Enter kaspa:address / block hash / transaction hash" />
+              <Button variant="primary" onClick={getBalance}>Go!</Button>
+              </div>
+              </Col>
+            </Row>
+            </Form>
+      </Container>
       <BalanceModal handleClose={handleClose} show={show} address={address} balance={balance} />
     </div>
   );
