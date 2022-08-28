3f077fe759424cb395ceedea95e4fa6728dbcb4d
lAmeR <42315864+lAmeR1@users.noreply.github.com>
2022-08-29 01:01:49 +0200

    every url on react app

--- server.js
+++ server.js
@@ -7,4 +7,9 @@ app.use(express.static(path.join(__dirname, 'build')));
 const port = process.env.PORT || '8080';
 app.set('port', port);
 const server = http.createServer(app);
+
+app.get('/*', (req, res, next) => {
+    // Return React App index.html
+  });
+
 server.listen(port, () => console.log(`Running on localhost:${port}`));
