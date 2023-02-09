80be033ff64f9f63f15a1725c3c82ff0b19c4da5
lAmeR <42315864+lAmeR1@users.noreply.github.com>
2023-02-09 22:40:28 +0100

    fixed utxo pagination

--- src/components/UtxoPagination.js
+++ src/components/UtxoPagination.js
@@ -22,14 +22,14 @@ const UtxoPagination = (props) => {
 
     return <div className="ms-auto fs-6">
         <Pagination>
-            {(props.active - 2) > 1 ? <>
+            {(Math.min(props.active, props.total - 2) - 2) > 1 ? <>
                 <Pagination.Item onClick={() => { props.setActive(1) }}>{1}</Pagination.Item>
-                <Pagination.Ellipsis />
+                {props.active > 4 && <Pagination.Ellipsis />}
             </> : <></>}
             {items}
 
-            {props.total > props.active + 2 ? <>
-                <Pagination.Ellipsis />
+            {props.total > Math.max(3, props.active) + 2 ? <>
+                {props.active < (props.total - 3) && <Pagination.Ellipsis />}
                 <Pagination.Item onClick={() => { props.setActive(props.total) }}>{props.total}</Pagination.Item>
             </> : <></>}
         </Pagination>
